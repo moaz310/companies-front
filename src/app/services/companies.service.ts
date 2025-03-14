59907d74f9578dd3be8b3b70eaf2b604fdd61dbb
moaz3101 <moazamar310@gmail.com>
2025-03-14 16:18:01 +0200

    add companies endpint calls

--- src/app/services/companies.service.ts
+++ src/app/services/companies.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { addCompanyUrl, deleteCompanyUrl, getCompaniesUrl, updateCompanyUrl } from '../constants/Urls';
+import { Observable } from 'rxjs';
+
+export interface Company{
+  id: number;
+  companyName: string;
+  companyAddress: string;
+  companyEmail: string;
+  companyPhone: string;
+  companyWebsite: string;
+  companyDescription: string;
+  companyLogoUrl: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompaniesService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getCompanies(): Observable<Company[]> {
+    return this.httpClient.get<Company[]>(getCompaniesUrl);
+  }
+
+  deleteCompany(id: number): Observable<any> {
+    return this.httpClient.delete<any>(deleteCompanyUrl + id);
+  }
+
+  updateCompany(company: Company): Observable<any> {
+    return this.httpClient.put<any>(updateCompanyUrl + company.id, company);
+  }
+
+  addCompany(company: Company): Observable<any> {
+    return this.httpClient.post<any>(addCompanyUrl, company);
+  }
 }
